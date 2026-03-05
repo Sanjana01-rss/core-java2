acd8d3c81fb45e41cc03603a01b049802eeef750
Sanjana01-rss <you@example.com>
2026-03-05 10:06:35 +0530

    task3

---
+++ task3/Bag.java
@@ -0,0 +1,62 @@
+class Bag
+{
+	static String product_dimensions;
+	static 	String date_first_available;
+	static 	String manufacturer;
+	static 	String asin;
+	static 	String item_model_number;
+	static 	String country_of_origin;
+	static 	String department;
+	static 	String manufacturer_location;
+	static 	String packer;
+	static 	String item_weight;
+	static 	String item_dimensions;
+	static 	String net_quantity;
+	static 	String generic_name;
+	static 	String best_sellers_rank;
+	public static void main(String[] args)
+	{
+		String product_dimensions="9 x 17 x 31 cm; 300 g  ";
+		String date_first_available=" 23 December 2025";
+		String manufacturer=" VALIJA Lifestyle, Thane, Maharashtra";
+		String asin="B0GC7GV6SH ";
+		String item_model_number="ATB-001-Black";
+		String country_of_origin="India";
+		String department="unisex-adult";
+		String manufacturer_location=" VALIJA Lifestyle, Thane, Maharashtra";
+		String packer="Valija Lifestyle, Surya Orchid, Thane, Maharashtra-401107, Contact-9326575846, Email-Valijalifestyle@gmail.com";
+		String item_weight="300 g";
+		String item_dimensions=" 9 x 17 x 31 Centimeters ";
+		String net_quantity=" 1 Count";
+		String generic_name=" Cross Body Bag";
+		String best_sellers_rank=" #133 in Bags, Wallets and Luggage (See Top 100 in Bags, Wallets and Luggage)#3 in Messenger & Sling Bags";
+		System.out.println(" Product Dimensions is : " + product_dimensions);
+		System.out.println(" Product Dimensions is : " + Bag.product_dimensions);
+		System.out.println(" Product Date First Available is : " + date_first_available);
+		System.out.println(" Product Date First Available is : " + Bag.date_first_available);
+		System.out.println(" Manufacturer is : " + manufacturer);
+		System.out.println(" Manufacturer is : " + Bag.manufacturer);
+		System.out.println(" ASIN is : " + asin);
+		System.out.println(" ASIN is : " +Bag.asin);
+		System.out.println(" Item Model Number is : " + item_model_number);
+		System.out.println(" Item Model Number is : " + Bag.item_model_number);
+		System.out.println(" Country Of Origin is : " + country_of_origin);
+		System.out.println(" Country Of Origin is : " + Bag.country_of_origin);
+		System.out.println(" Department is : " + department);
+		System.out.println(" Department is : " + Bag.department);
+		System.out.println(" Manufacturer is : " + manufacturer_location);
+		System.out.println(" Manufacturer is : " + Bag.manufacturer_location);
+		System.out.println(" PACKER is : " + packer);
+		System.out.println(" PACKER is : " + Bag.packer);
+		System.out.println(" Item Weight is : " + item_weight);
+		System.out.println(" Item Weight is : " + Bag.item_weight);
+		System.out.println(" Item Dimensions is : " + item_dimensions);
+		System.out.println(" Item Dimensions is : " + Bag.item_dimensions);
+		System.out.println(" Net Quantity is : " + net_quantity);
+		System.out.println(" Net Quantity is : " + Bag.net_quantity);
+		System.out.println(" Generic Name is : " + generic_name);
+		System.out.println(" Generic Name is : " + Bag.generic_name);
+		System.out.println(" Best sellers Rank is : " + best_sellers_rank);
+		System.out.println(" Best sellers Rank is : " + Bag.best_sellers_rank);
+	}
+}
