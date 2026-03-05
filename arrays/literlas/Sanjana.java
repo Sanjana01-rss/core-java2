082c21a24b1c187791ae4e09bd3ae702058daa57
Sanjana01-rss <you@example.com>
2026-03-05 10:18:16 +0530

    arrays

---
+++ arrays/literlas/Sanjana.java
@@ -0,0 +1,12 @@
+class Sanjana
+{ 
+	public static int main(String[] human)
+	{
+		String karnataka="Karnataka";
+		String kerala="Kerala";
+		String tamilNadu="Tamil Nadu";
+		String properties[]={karnataka,kerala,tamilNadu};
+		for(String property: properties)
+			System.out.println(property);
+	}
+}
